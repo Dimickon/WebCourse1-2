a13410030acfb6259b9d209e7e803ba449d9e295
Dimickon <dimickon1@gmail.com>
2022-06-03 00:40:39 +0300

    beta 1.1
    Закончена страница с рейтингом. Сделан адаптив.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,12 @@
-console.log("Hello!");
+$(document).ready(function() {
+    $('.rating__categories').click(function(e){
+        e.preventDefault();
+
+        $('.rating__categories').removeClass('rating__categories--active');
+        $('.rating__diagram').removeClass('rating__diagram--active');
+
+        $(this).addClass('rating__categories--active');
+        $($(this).attr('href')).addClass('rating__diagram--active');
+    });
+    $('.rating__categories:first').click();
+});
