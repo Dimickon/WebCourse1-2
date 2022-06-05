7f527030a047fa56d45bb181c5cc1ae5be72064c
Dimickon <dimickon1@gmail.com>
2022-06-05 21:26:34 +0300

    beta 1.2

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
+    let intro = $("#intro");
+    let header = $('#header');
+    let introH = intro.innerHeight();
+    let headerH = header.innerHeight();
+    let scrollTop = $(window).scrollTop();
+
+    /* Header class on scroll
+    =====================================*/
+
+    headerScroll();
+
+    $(window).on("scroll  resize", function() {
+        headerScroll();
+    });
+
+    function headerScroll() {
+        introH = intro.innerHeight();
+        headerH = header.innerHeight();
+
+        let scrollTop = $(this).scrollTop();
+
+        if( scrollTop >= (introH - headerH) ) {
+            header.addClass("header--dark");
+        } else {
+            header.removeClass("header--dark");
+        }
+    }
+
+    /* Categories
+    =====================================*/
+
     $('.rating__categories').click(function(e){
         e.preventDefault();
 
